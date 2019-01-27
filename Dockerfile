75ffcb5a7b4b4e706803151b719aa599ea5bd69d
Jacob J. Zhang <jacobjzhang@gmail.com>
2019-01-27 10:19:09 -0500

    Moving to gcloud storage for static items and updating reference

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ ENV VIRTUAL_ENV /env
 ENV PATH /env/bin:$PATH
 ADD requirements.txt /app/
 RUN pip install -r requirements.txt
-ADD . /app/
 CMD exec python manage.py collectstatic
+ADD . /app/
 CMD exec gunicorn -b :$PORT mipa_backend.wsgi
--- mipa_backend/settings.py
+++ mipa_backend/settings.py
@@ -140,4 +140,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'mipa_backend/app/static')
-STATIC_URL = '/static/'
+STATIC_URL = 'https://storage.googleapis.com/mipa-backend/static/'
